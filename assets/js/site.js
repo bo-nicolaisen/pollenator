1f07201fbb18256763098d240e8cc21a6c6f8bb7
Bo Nicolajsen <>
2024-03-21 11:06:34 +0100

    start on pollen data

--- assets/js/site.js
+++ assets/js/site.js
@@ -12,8 +12,9 @@ function getLocation() {
         //  navigator.geolocation.getCurrentPosition requires a succes function name as first param and a error function name as second param.
 
         navigator.geolocation.getCurrentPosition(PositionRecieved, geoError);
+
     } else {
-        //x.innerHTML = "Geolocation is not supported by this browser.";
+        alert("Geolocation is not supported by this browser.")
     }
 }
 
@@ -23,7 +24,11 @@ function PositionRecieved(position) {
     console.log(position.coords.longitude);
     console.log(position.coords.latitude);
 
+    // get location name
     GetHumanReadableLocation(position.coords.latitude, position.coords.longitude)
+
+    // get pollen data on location
+    GetPollenData(position.coords.latitude, position.coords.longitude)
 }
 
 
@@ -66,6 +71,50 @@ function GetHumanReadableLocation(lat, long) {
             return null;
         });
 }
+
+
+
+function GetPollenData(lat, long) {
+
+    const apiKey = "65fb5ea644244903025253axe09afbb";
+    const url = `https://geocode.maps.co/reverse?lat=${lat}&lon=${long}&api_key=${apiKey}`;
+
+    console.log("get pollen data");
+    console.log(lat, long);
+
+    /*  fetch(url)
+ 
+         .then(response => {
+ 
+ 
+ 
+             if (!response.ok) {
+                 throw new Error('Network response was not ok');
+             }
+ 
+ 
+             return response.json();
+         })
+         .then(data => {
+ 
+ 
+             BuildlocationName(data.address.city)
+ 
+         })
+         .catch(error => {
+             console.error('Error fetching data:', error);
+             return null;
+         }); */
+}
+
+
+
+
+
+
+
+
+
 
 
 
