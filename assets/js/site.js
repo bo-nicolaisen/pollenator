15cba889c658a7ba9bc94f6273a1b9357bffe89b
Bo Nicolajsen <>
2024-03-21 13:24:22 +0100

    dataconversion and view

--- assets/js/site.js
+++ assets/js/site.js
@@ -76,37 +76,83 @@ function GetHumanReadableLocation(lat, long) {
 
 function GetPollenData(lat, long) {
 
-    const apiKey = "65fb5ea644244903025253axe09afbb";
-    const url = `https://geocode.maps.co/reverse?lat=${lat}&lon=${long}&api_key=${apiKey}`;
+
+
+    /*  https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${long}&current=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&timezone=${timeZone}&forecast_days=1
+  */
+    const timeZone = "Europe%2FBerlin";
+
+    const url = ` https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${long}&current=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&timezone=${timeZone}&forecast_days=1`;
+
 
     console.log("get pollen data");
     console.log(lat, long);
 
-    /*  fetch(url)
- 
-         .then(response => {
- 
- 
- 
-             if (!response.ok) {
-                 throw new Error('Network response was not ok');
-             }
- 
- 
-             return response.json();
-         })
-         .then(data => {
- 
- 
-             BuildlocationName(data.address.city)
- 
-         })
-         .catch(error => {
-             console.error('Error fetching data:', error);
-             return null;
-         }); */
+    fetch(url)
+
+        .then(response => {
+
+
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+
+            return response.json();
+        })
+        .then(data => {
+
+
+            pollenDataScructure(data)
+
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            return null;
+        });
 }
 
+
+// controller
+function pollenDataScructure(data) {
+
+    let myViewData = []
+
+    // data about current values
+    myViewData.push(data.current)
+
+
+
+    BuildPollenView(myViewData)
+
+
+}
+
+
+// view code
+
+function BuildPollenView(viewData) {
+
+    // build current
+    let myDisplayElement = document.getElementById('PollenData')
+
+    //console.log(viewData[0]);
+
+    let myCurrentData = viewData[0]
+    // generate Card HTML for current values
+    let myCurrentHTML = `<section id="currentValues"><h2>Pollental</h2><ul>
+                <li>El ${myCurrentData.alder_pollen}</li>
+                <li>Birk ${myCurrentData.birch_pollen}</li>
+                <li>Græs ${myCurrentData.grass_pollen}</li>
+                <li>Bynke ${myCurrentData.mugwort_pollen}</li>
+                 <li>Oliven ${myCurrentData.olive_pollen}</li>
+                   <li>Ambrosia ${myCurrentData.ragweed_pollen}</li>
+            </ul>
+        </section>`
+
+    myDisplayElement.innerHTML = myCurrentHTML
+}
 
 
 
@@ -123,7 +169,7 @@ function BuildlocationName(myCity) {
 
     console.log(myCity);
 
-    let myNameElement = document.getElementById("app")
+    let myNameElement = document.getElementById("location")
 
     myNameElement.innerText = myCity
 
