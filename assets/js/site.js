6181b858d6417de49b2d8caa494546e3be633879
bo-nicolaisen <bni@techcollege.dk>
2024-04-10 13:23:53 +0200

    rebase
    med map

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,223 +1,202 @@
 // write cool JS hwere!!
 
-getLocation()
+let map
+let PopUp=false;
+
+getLocation();
 
 
-function getLocation() {
-
-    if (navigator.geolocation) {
-
-        //  navigator.geolocation.getCurrentPosition requires a succes function name as first param and a error function name as second param.
-
-        navigator.geolocation.getCurrentPosition(PositionRecieved, geoError);
-
-    } else {
-        alert("Geolocation is not supported by this browser.")
-    }
-}
-
-// Geo location succes function recieves a data object 
-function PositionRecieved(position) {
-    //console.log(position);
-
-    // get location name
-    GetHumanReadableLocation(position.coords.latitude, position.coords.longitude)
-
-    // get pollen data on location
-    GetPollenData(position.coords.latitude, position.coords.longitude)
-}
-
-//geo error function recievs a data object
-function geoError(error) {
-
-    console.log(error.message);
+/* function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(GetAll);
+   
+  } else {
+    document.getElementById("demo").innerHTML = "Geolocation is not supported";
+  }
 }
 
 
-function GetHumanReadableLocation(lat, long) {
-
-    const apiKey = "65fb5ea644244903025253axe09afbb";
-    const url = `https://geocode.maps.co/reverse?lat=${lat}&lon=${long}&api_key=${apiKey}`;
-
-    fetch(url)
-
-        .then(response => {
+ async function GetAll(position) {
 
 
 
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
 
-            return response.json();
-        })
-        .then(data => {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
 
+    makeMap(latitude, longitude)
 
-            BuildlocationName(data.address.city)
+    let myUrl = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${latitude}&longitude=${longitude}&current=alder_pollen,birch_pollen,grass_pollen,ragweed_pollen&hourly=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,ragweed_pollen&timezone=Europe%2FBerlin&domains=cams_europe`;
 
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            return null;
-        });
-}
+    let geoUrl=`https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}&api_key=65fb5ea644244903025253axe09afbb`
 
-function GetPollenData(lat, long) {
+  try {
+    const res = await Promise.all([
+      fetch(myUrl),
+      fetch(geoUrl)
+    ]);
+    const data = await Promise.all(res.map(r => r.json()))
 
-    //to do get timezone from date object
-    const timeZone = "Europe%2FBerlin";
+    console.log(data[0]);
+    console.log(data[1]);
 
-    const url = ` https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${long}&current=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&timezone=${timeZone}&forecast_days=1`;
+  } catch {
+    throw Error("Promise failed");
+  }
+}; */
+ 
 
+ function makeMap(latitude, longitude) {
 
-    fetch(url)
+   map = L.map('map').setView([latitude, longitude], 13);
 
-        .then(response => {
-
-
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-
-            return response.json();
-        })
-        .then(data => {
-
-
-            pollenDataScructure(data)
-
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            return null;
-        });
+  //var marker = L.marker([latitude, longitude]).addTo(map);
+  map.on('click', onMapClick);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 }
 
 
-// controller
-function pollenDataScructure(data) {
 
-    let myViewData = []
+function onMapClick(e) {
+ 
+  //moveMapToMarker(10,10);
 
-    // data about current values
-    myViewData.push(data.current)
-
-
-    // generate Hour data
-
-    let myHourlyData = []
-
-    console.log(data.hourly);
+ console.log(e.latlng);
 
 
-    data.hourly.time.map((myTime, index) => {
-        let myHourData = {}
-
-        myHourData.time = myTime
-
-        myHourData.alder_pollen = data.hourly.alder_pollen[index]
-        myHourData.birch_pollen = data.hourly.birch_pollen[index]
-
-        myHourData.grass_pollen = data.hourly.grass_pollen[index]
-        myHourData.mugwort_pollen = data.hourly.mugwort_pollen[index]
-        myHourData.olive_pollen = data.hourly.olive_pollen[index]
-        myHourData.ragweed_pollen = data.hourly.ragweed_pollen[index]
-
-
-        console.log("myHourData: " + myHourData);
-        myHourlyData.push(myHourData)
-
-    })
-
-    console.log(myHourlyData);
-
-
-
-
-
-    /*   let HourData = []
-      data.hourly.time.map((myTime, index) => {
-          let hourData = {}
-          hourData.time = myTime
-          hourData.alder_pollen = data.hourly.alder_pollen[index]
-          hourData.birch_pollen = data.hourly.birch_pollen[index]
-          hourData.grass_pollen = data.hourly.grass_pollen[index]
-          hourData.mugwort_pollen = data.hourly.mugwort_pollen[index]
-          hourData.olive_pollen = data.hourly.olive_pollen[index]
-          hourData.ragweed_pollen = data.hourly.ragweed_pollen[index]
   
-          HourData.push(hourData)
-      })
-      myViewData.push(HourData)
-      //console.log(HourData); */
 
-    BuildPollenView(myViewData)
+  let myContent=`<p>Hello world!<br />This is a nice popup.</p><button onClick="MapPopupCallBack(${e.latlng.lat},${e.latlng.lng})">ok</button>`
+  PopUp = L.popup(e.latlng, {content: myContent}).openOn(map);
+ 
+  /* var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+  getPollenData(e.latlng.lat,e.latlng.lng)  */ 
+} 
+
+
+function moveMapToMarker(latitude,longitude){
+
+  
+latitude=57.35
+longitude=9.950001
+
+  map.setView([latitude, longitude], 13);
+ 
+}
+
+
+
+
+ function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(positionSucces);
+   
+  } else {
+    document.getElementById("demo").innerHTML = "Geolocation is not supported";
+  }
+} 
+
+
+
+function positionSucces(position){
+  makeMap(position.coords.latitude, position.coords.longitude);
+  getPollenData(position.coords.latitude, position.coords.longitude)
+
+}
+
+
+function getPollenData(latitude,longitude) {
+
+ 
+/* console.log("get pollen data: "+position);
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude; */
+
+  getLocationName(latitude,longitude);
+
+  let myUrl = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${latitude}&longitude=${longitude}&current=alder_pollen,birch_pollen,grass_pollen,ragweed_pollen&hourly=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,ragweed_pollen&timezone=Europe%2FBerlin&domains=cams_europe`;
+
+  fetch(myUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Handle the data
+      //console.log(data);
+      PollenDataRecieved(data)
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the fetch
+      console.error("Fetch error:", error);
+    });
+}
+
+
+function getLocationName(lat,long){
+
+
+
+
+  fetch(`https://geocode.maps.co/reverse?lat=${lat}&lon=${long}&api_key=65fb5ea644244903025253axe09afbb`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Handle the data
+      console.log(data.display_name);
+      //console.log(data.address.hamlet+' '+data.address.village);
+    
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the fetch
+      console.error("Fetch error:", error);
+    });
 
 
 }
 
 
 
+function navCallBack(myNavItem){
 
-// view code
-
-// builds a pollen data view with current data and hourly 24 hor data recieved in an array
-function BuildPollenView(viewData) {
-
-    // build current section
-    let myDisplayElement = document.getElementById('PollenData')
-
-    let myCurrentData = viewData[0]
-    // generate Card HTML for current values
-    let myCurrentHTML = `<section id="currentValues"><h2>Pollental</h2><ul>
-                <li>El ${myCurrentData.alder_pollen}</li>
-                <li>Birk ${myCurrentData.birch_pollen}</li>
-                <li>Græs ${myCurrentData.grass_pollen}</li>
-                <li>Bynke ${myCurrentData.mugwort_pollen}</li>
-                 <li>Oliven ${myCurrentData.olive_pollen}</li>
-                   <li>Ambrosia ${myCurrentData.ragweed_pollen}</li>
-            </ul>
-        </section>`
-
-    myDisplayElement.innerHTML = myCurrentHTML
-
-
-    // build hours from HourData viewData[1]
-
-    /*  let myHourViewHTML = '<section id="hours"><h2>time visning</h2>'
- 
-     let myHourdata = viewData[1]
- 
-     myHourdata.map((myHour) => {
-         let myCurrentHTML = `<section class="hourcard"><h3>${myHour.time}</h3><ul>
-                 <li>El ${myHour.alder_pollen}</li>
-                 <li>Birk ${myHour.birch_pollen}</li>
-                 <li>Græs ${myHour.grass_pollen}</li>
-                 <li>Bynke ${myHour.mugwort_pollen}</li>
-                  <li>Oliven ${myHour.olive_pollen}</li>
-                    <li>Ambrosia ${myHour.ragweed_pollen}</li>
-             </ul>
-         </section>`
-         myHourViewHTML += myCurrentHTML
-     })
- 
- 
-     myHourViewHTML += '</section>'
-     myDisplayElement.innerHTML += myHourViewHTML */
-}
-
-
-// sets location name in dom
-function BuildlocationName(myCity) {
-
-    //console.log(myCity);
-    let myNameElement = document.getElementById("location")
-    myNameElement.innerText = myCity
+  switch (myNavItem) {
+    case "map":
+      console.log("map");
+      break;
+      case "settings":
+      console.log("settings");
+      break;
+      case "home":
+      console.log("home");
+      break;
+      default:
+     
+        break;
+  }
 
 }
 
+function MapPopupCallBack(lat,lng){
+  console.log("pop up");
+  map.closePopup(PopUp)
+  PopUp=false;
 
+  var marker = L.marker([lat,lng]).addTo(map).bindPopup("Saved Location");
+  getPollenData(lat,lng) 
 
+}
+
+function PollenDataRecieved(data){
+
+  console.log(data);
+
+}
